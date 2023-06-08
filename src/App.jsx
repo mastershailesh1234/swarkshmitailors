43d731b35d27047bad2a40718432d42a311f5e72
Shailesh Patro <shailesh.patro2002@gmail.com>
2023-06-09 01:42:12 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import Herosection from "./components/Herosection/Herosection";
 import Navbar from "./components/Navbar/Navbar";
-
+import Footer from "./components/Footer/Footer";
+import AboutUs from "./components/AboutUs/AboutUs";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Pricing from "./components/Pricing/Pricing";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar />
-      <Herosection />
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
